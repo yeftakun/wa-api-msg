9749841d21510adba2656992d9e6f920597ecb70
yeftakun <yeftakun34@gmail.com>
2025-04-26 18:22:30 +0800

    brmmm

---
+++ index.js
@@ -0,0 +1,56 @@
+require('dotenv').config(); // Tambahkan ini paling atas
+
+const { Client, LocalAuth } = require('whatsapp-web.js');
+const readline = require('readline');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+const client = new Client({
+    authStrategy: new LocalAuth()
+});
+
+client.on('qr', (qr) => {
+    console.log('Scan QR ini untuk login WhatsApp:');
+    require('qrcode-terminal').generate(qr, { small: true });
+});
+
+client.on('ready', async () => {
+    console.log('Bot sudah terhubung!');
+});
+
+// Middleware cek API Key
+app.use((req, res, next) => {
+    const apiKey = req.headers['x-api-key'];
+
+    if (apiKey !== process.env.API_KEY) {
+        return res.status(401).json({ status: 'error', message: 'Unauthorized' });
+    }
+
+    next();
+});
+
+app.post('/send-message', async (req, res) => {
+    const { number, message } = req.body;
+    const targetNumber = `${number}@c.us`;
+
+    const isRegistered = await client.isRegisteredUser(targetNumber);
+    if (isRegistered) {
+        try {
+            await client.sendMessage(targetNumber, message);
+            res.status(200).json({ status: 'success', message: `Terkirim ke ${number}` });
+        } catch (error) {
+            res.status(500).json({ status: 'error', message: `Gagal terkirim ke ${number}` });
+        }
+    } else {
+        res.status(404).json({ status: 'error', message: `${number} tidak ada di WhatsApp.` });
+    }
+});
+
+client.initialize();
+
+app.listen(3000, () => {
+    console.log('Server berjalan di http://localhost:3000');
+});
---
+++ little-doc.md
@@ -0,0 +1,13 @@
+NPM yang digunakan di proyek ini adalah `npm@6.14.18` yang diinstall secara lokal project, karena saya menemukan kalau menggunakan `npm@10.5.0` library `whatsapp-web.js` tidak bisa terinstall.
+
+Menggunakan npm secara lokal:
+
+```
+npx npm ...
+```
+
+curl:
+
+```
+curl -X POST http://localhost:3000/send-message -H "Content-Type: application/json" -d "{\"number\":\"628xxx\",\"message\":\"Halo dari curl!\"}"
+```
